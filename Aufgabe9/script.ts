288ed2bcdbc1036231c0833997e72e385a057f15
puderemm <emma.arabella.puder@hs-furtwangen.de>
2020-06-28 17:55:37 +0200

    Aufgabe9

---
+++ Aufgabe9/script.ts
@@ -0,0 +1,104 @@
+window.addEventListener('load', function () {
+
+
+   
+
+    var liste: string[] = ["BWL", "EIA", "Audiotechnik"];
+
+    document.querySelector("#items").innerHTML = liste.length + " in total";  
+
+    function drawList() {
+        document.querySelector("#todo").innerHTML = "";
+        for (let index = 0; index < liste.length; index++) {
+            console.log(liste[index]);
+            document.querySelector("#todo").innerHTML += "<p>" + "<input type='checkbox'>" + liste[index] + "<i class='fas fa-trash'></i>" +"</p>";
+
+        }
+    }
+
+
+    document.querySelector("#newTodoButton").addEventListener("click", addnewElement);
+
+
+    function addnewElement() {
+        var neuerEintrag: string = document.querySelector("#eingabe").value;
+        liste.push(neuerEintrag);
+        drawList();
+        document.querySelector("#eingabe").value = "";
+
+        var span = document.createElement("SPAN");
+        var mull = document.createElement("i");
+        mull.className = "fas fa-trash"
+        span.className = "close";
+        span.appendChild(mull);
+        list.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+              var div = this.parentElement;
+              div.style.display = "none";
+            }
+          }
+    }
+ 
+
+    drawList ()
+
+
+
+    var myNodelist = document.getElementsByTagName("p");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var mull = document.createElement("i");
+  mull.className = "fas fa-trash"
+  span.className = "close";
+  span.appendChild(mull);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+ /*   
+document.getElementById("trash").addEventListener("click", Elementtrash);
+
+
+
+function Elementtrash () {
+    console.log("hi");
+}
+
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+    id--;
+    document.querySelector("#counter").innerHTML = ("Total: " + id);
+}
+list.addEventListener("click", function (event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+    if (elementJob == "complete") {
+        completeToDo(element);
+    }
+    else if (elementJob == "delete") {
+        removeToDo(element);
+    }
+});
+  */
+
+})
